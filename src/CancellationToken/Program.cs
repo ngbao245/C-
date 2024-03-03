2aeae820a2c59e9d00ff2b848a5d0753c61ab20f
BaoBiBo <ng.hoangbao03@gmail.com>
2024-03-03 16:03:51 +0700

    learned Thread and CancellationToken

---
+++ src/CancellationToken/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Example
+{
+    static async Task Main()
+    {
+        // Tạo đối tượng CancellationTokenSource
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        // Truyền CancellationToken từ CancellationTokenSource vào phương thức
+        Task<string> task = CreateAsync("example", cts.Token);
+
+        // Sau 8 giây, hủy bỏ tác vụ
+        await Task.Delay(8000);
+        cts.Cancel();
+
+        try
+        {
+            // Đợi tác vụ hoàn thành
+            string result = await task;
+            Console.WriteLine(result);
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Tac vu da bi huy bo.");
+        }
+    }
+
+    static async Task<string> CreateAsync(string entity, CancellationToken cancellationToken = default)
+    {
+        // Kiểm tra trạng thái hủy bỏ
+        cancellationToken.ThrowIfCancellationRequested();
+
+        // Giả lập công việc không đồng bộ
+        await Task.Delay(2000, cancellationToken);
+
+        // Trả về kết quả
+        return $"Created {entity}";
+    }
+}
---
+++ src/ThreadTask/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            var x = i;
+            Thread a = new Thread(() =>
+            {
+                Console.WriteLine(i);
+            });
+            a.Start();
+            Thread.Sleep(3);
+        }
+        Console.WriteLine();
+    }
+}
+
+
